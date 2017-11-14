6e6717b4b27f421582088fafeb59b9f71b243617
bambusek7 <bambusek7@gmail.com>
2017-11-14 22:43:18 +0100

    mine node global modules

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,4 @@
 if test ! $(which spoof)
 then
-  sudo npm install spoof -g
+  sudo npm install npm nodemon create-react-app jest eslint babel-eslint eslint-config-defaults eslint-plugin-react eslint-plugin-jest forever http-server pm2 tldr typings webpack webpack-dev-server autocannon -g
 fi
