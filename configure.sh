8f7b53f471c517cf5644cd7d0abdec3841aa3815
undernotic <undernotic@gmail.com>
2017-11-16 18:49:29 +0100

    font sudo

--- configure.sh
+++ configure.sh
@@ -52,8 +52,8 @@ git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/theme
 echo ''
 echo "Now installing nerd-fonts..."
 echo ''
-mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+sudo mkdir -p ~/.local/share/fonts
+sudo cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 sudo fc-cache -fv
 
 # Midnight commander install
