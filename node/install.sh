58445dffd00f15c312f9e1f7fb452ef809da993d
undernotic <undernotic@gmail.com>
2018-06-10 17:34:24 +0200

    gifski for gif quality

--- node/install.sh
+++ node/install.sh
@@ -15,4 +15,4 @@ source ~/.nvm/nvm.sh
 nvm install node
 nvm use node
 
-npm install -g nodemon create-react-app jest eslint babel-eslint eslint-config-defaults eslint-plugin-react eslint-plugin-jest forever http-server pm2 tldr typescript webpack webpack-dev-server artillery markdown-to-medium testcafe yarn
+npm install -g nodemon create-react-app jest eslint babel-eslint eslint-config-defaults eslint-plugin-react eslint-plugin-jest forever http-server pm2 tldr typescript webpack webpack-dev-server artillery markdown-to-medium testcafe yarn gifski
