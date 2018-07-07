4706a64ef27e4a793fd2b3abe59b8075db238f92
undernotic <undernotic@gmail.com>
2018-07-07 20:36:11 +0200

    wo

---
+++ system/aliases.zsh
@@ -0,0 +1,5 @@
+alias reload!='. ~/.zshrc'
+alias cls='clear' # Good 'ol Clear Screen command
+
+alias xd='xdg-open'
+alias l='ls -lah --group-directories-first'
--- vscode/install.sh
+++ vscode/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
 mkdir -p ~/.config/Code/User
-yes | cp -f ~/.dotfiles/vscode/settings.json ~/.config/Code/Userch
+yes | cp -f ~/.dotfiles/vscode/settings.json ~/.config/Code/User
