f21509b4209370ad64e51434fa5fe9b046b50c43
undernotic <undernotic@gmail.com>
2018-07-10 00:18:39 +0200

    conky autostart

--- conky/install.sh
+++ conky/install.sh
@@ -5,6 +5,5 @@ echo "Now installing conky..."
 echo ''
 sudo apt install conky -y
 
-sudo cp ./conky/conky.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable conky.service
+mkdir -p ~/.config/autostart
+cp ./conky/conky.desktop ~/.config/autostart
