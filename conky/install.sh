b9473cb147d72e1a76783390defbf24cf5affa2f
undernotic <undernotic@gmail.com>
2018-07-10 00:06:55 +0200

    run conky at start

--- conky/install.sh
+++ conky/install.sh
@@ -4,3 +4,7 @@ echo ''
 echo "Now installing conky..."
 echo ''
 sudo apt install conky -y
+
+sudo cp ./conky/conky.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable conky.service
