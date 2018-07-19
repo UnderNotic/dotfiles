514793612be2f8ce350311a0b481d12e9d349eaf
undernotic <undernotic@gmail.com>
2018-07-19 20:12:19 +0200

    not needed fonts, gnome tweaks

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# fonts for powerline install (change terminal to use sourcecodepro-powerline-awesome-regular font)
 echo ''
 echo "Now installing fonts..."
 echo ''
@@ -8,3 +7,9 @@ sudo mkdir -p /usr/share/fonts/added
 yes | sudo cp -rf ./fonts/files/* /usr/share/fonts/added
 sudo apt install fonts-roboto -y
 sudo fc-cache -fv
+
+# Gnome tweaks to change Monospace font to SauceCodeProRegular and terminal to use NOT custom font
+echo ''
+echo 'Now installing gnome tweaks'
+echo ''
+sudp apt install gnome-tweaks -y
