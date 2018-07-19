a359b4625f4e6c87ace46f57486034c4078ad410
undernotic <undernotic@gmail.com>
2018-07-19 10:37:24 +0200

    chrome

--- configure.sh
+++ configure.sh
@@ -107,6 +107,15 @@ sudo add-apt-repository ppa:peek-developers/stable  -y
 sudo apt update
 sudo apt install peek -y
 
+# Chrome
+echo ''
+echo 'Now installing chrome'
+echo ''
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt update
+sudo apt install google-chrome-stable -y
+
 echo ''
 echo 'Now installing ripgrep, fd and fzf'
 echo ''
