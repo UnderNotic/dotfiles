0aabbdb3e02c89615541e8e0fbbdf705f6199d5c
undernotic <undernotic@gmail.com>
2018-07-30 23:11:43 +0200

    self signed local cert generator

---
+++ local_cert/generate_cert.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout localhost.key -out localhost.crt -config localhost.conf -passin pass:YourSecurePassword
+
+sudo openssl pkcs12 -export -out localhost.pfx -inkey localhost.key -in localhost.crt
