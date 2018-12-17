f65841adc6502a2c50412ca9f31af85548143b7e
undernotic <undernotic@gmail.com>
2018-12-17 20:28:28 +0100

    new dotnet

--- dotnet/install.sh
+++ dotnet/install.sh
@@ -11,7 +11,7 @@ sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
 sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
 sudo apt update
-sudo apt install dotnet-sdk-2.1 -y
+sudo apt install dotnet-sdk-2.2 -y
 
 # Setup and configure az cli
 echo ''
