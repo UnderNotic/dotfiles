6489c6a4676011c36fbc450322855416b1bb00fb
undernotic <undernotic@gmail.com>
2019-01-12 16:56:58 +0100

    nvm default packages

--- node/install.sh
+++ node/install.sh
@@ -4,7 +4,13 @@
 echo ''
 echo 'Installing nvm'
 echo ''
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+#setup nvm default global packages
+echo ''
+echo 'Setuping nvm default global packages'
+echo ''
+cp ./node/default-packages ~/.nvm/
 
 # node install
 echo ''
@@ -14,12 +20,3 @@ source ~/.nvm/nvm.sh
 
 nvm install node
 nvm use node
-
-# npm install
-echo ''
-echo 'Installing npm global packages'
-echo ''
-npm install -g nodemon create-react-app jest forever http-server pm2 webpack webpack-dev-server artillery testcafe yarn gatsby-cli fkill-cli
-npm install -g regexgen clipboard-cli tldr markdown-to-medium gifski
-npm install -g eslint babel-eslint eslint-config-defaults eslint-config-standard eslint-config-standard-react eslint-plugin-standard eslint-plugin-promise eslint-plugin-jest eslint-plugin-import eslint-plugin-node eslint-plugin-react tslint typescript
-# npm install -g ndb
