402e972218e4af7f7b02295a4525f02b8c0b70fd
undernotic <undernotic@gmail.com>
2019-11-12 22:50:35 +0100

    use normal nvm loading

--- node/nvm_load.zsh
+++ node/nvm_load.zsh
@@ -1,3 +1,4 @@
+# this commented code speeds up zsh startup, but it also fights with stuff using node (for example vscode jest won't work with this approach)
 declare -a NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
 
 NODE_GLOBALS+=("node")
@@ -11,3 +12,8 @@ load_nvm () {
 for cmd in "${NODE_GLOBALS[@]}"; do
     eval "${cmd}(){ unset -f ${NODE_GLOBALS}; load_nvm; ${cmd} \$@ }"
 done
+
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
