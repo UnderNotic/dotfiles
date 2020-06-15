0a3dcfd32aaf615063ed27bfc1033ebfec884da3
undernotic <undernotic@gmail.com>
2020-06-15 12:18:41 +0200

    added albert

---
+++ albert/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# albert install
+echo ''
+echo 'Installing albert'
+echo ''
+
+echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
+sudo apt update
+sudo apt install albert -y
+
+mkdir -p ~/.config/albert
+cp ./albert/albert.conf ~/.config/albert
