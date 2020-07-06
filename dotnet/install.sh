b211c1bb992df61da2191d2d0d61d58057aded45
undernotic <undernotic@gmail.com>
2020-07-07 01:22:46 +0200

    dotnet install

--- dotnet/install.sh
+++ dotnet/install.sh
@@ -4,14 +4,11 @@
 echo ''
 echo 'Installing dotnet core'
 echo ''
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
-sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/prod.list 
-sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
-sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
-sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo rm packages-microsoft-prod.deb
 sudo apt update
-sudo apt install dotnet-sdk-3. -y
+sudo apt-get install -y dotnet-sdk-3.1
 
 # Setup and configure az cli
 echo ''
