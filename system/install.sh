7eb5cc8d632e10dfa45d7347dce2608cbc18c7e3
undernotic <undernotic@gmail.com>
2020-09-05 18:45:02 +0200

    removing not needed vscode fix

--- system/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo ''
-echo 'Installing system wide environment variables'
-echo ''
-sudo touch /etc/profile.d/env_variables.sh
-sudo echo 'export GTK_IM_MODULE="xim"' > /etc/profile.d/env_variables.sh
