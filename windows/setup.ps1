64e550ebcfc243f85f64fcbbb73a0f75a184327a
undernotic <undernotic@gmail.com>
2020-11-23 01:15:42 +0100

    fix paths

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -5,8 +5,8 @@ mkdir C:\workspace\open_source
 mkdir C:\workspace\work
 
 # git
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UnderNotic/dotfiles/windows-wsl/windows/git/.gitconfig" -OutFile "C:\file"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UnderNotic/dotfiles/windows-wsl/windows/git/.gitconfig-work" -OutFile "C:\file2"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UnderNotic/dotfiles/windows-wsl/windows/git/.gitconfig" -OutFile $HOME\.gitconfig
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UnderNotic/dotfiles/windows-wsl/windows/git/.gitconfig-work" -OutFile "C:\workspace\work\.gitconfig-work"
 
 # choco install
 iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/UnderNotic/dotfiles/windows-wsl/windows/choco/install.ps1'))
