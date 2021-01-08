da501fd76f1bc2bde8101135674161db2e504e05
undernotic <undernotic@gmail.com>
2021-01-08 21:20:30 +0100

    fix hotkey pull

--- windows/auto-hotkey/install.ps1
+++ windows/auto-hotkey/install.ps1
@@ -1,1 +1,3 @@
-﻿Invoke-WebRequest -Uri "https://github.com/UnderNotic/dotfiles/raw/windows-wsl/windows/auto-hotkey/terminal.ahk" -OutFile "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\terminal.ahk"
+﻿$subpath = '\Microsoft\Windows\Start Menu\Programs\Startup\terminal.ahk'
+$outpath = $env:APPDATA + $subpath
+Invoke-WebRequest -Uri "https://github.com/UnderNotic/dotfiles/raw/windows-wsl/windows/auto-hotkey/terminal.ahk" -OutFile $outpath
