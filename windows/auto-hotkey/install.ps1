f1aa62fe2e565149e5f3fe2c05eaf8298a72f363
undernotic <undernotic@gmail.com>
2021-01-08 21:35:34 +0100

    fix autohotkey

--- windows/auto-hotkey/install.ps1
+++ windows/auto-hotkey/install.ps1
@@ -1,2 +1,2 @@
-﻿$outpath = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp'
+﻿$outpath = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\terminal.ahk'
 Invoke-WebRequest -Uri "https://github.com/UnderNotic/dotfiles/raw/windows-wsl/windows/auto-hotkey/terminal.ahk" -OutFile $outpath
