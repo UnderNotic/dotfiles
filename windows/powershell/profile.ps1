7a72dcbaf318da5d6546ae60893dbcd6c894cc98
undernotic <undernotic@gmail.com>
2021-03-05 21:35:44 +0100

    ps aliases

---
+++ windows/powershell/profile.ps1
@@ -0,0 +1,22 @@
+notepad $profile and copy following
+
+
+function gs()
+{
+    git status -sb
+}
+function gl(){
+    git pull
+}
+function glog(){
+    git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative
+}
+function gp(){
+    git push origin HEAD
+}
+function gd(){
+    git diff
+}
+function gl(){
+    git pull
+}
