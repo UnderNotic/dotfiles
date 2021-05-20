8cd4cbb5e4746ca1b7af44421d5ad011eefeef1a
undernotic <undernotic@gmail.com>
2021-05-20 13:44:58 +0200

    add agenttransack

--- windows/choco/install.ps1
+++ windows/choco/install.ps1
@@ -32,6 +32,7 @@ choco install pgptool -y
 choco install sharex -y
 choco install copyq -y
 # choco install everything -y optional
+choco install agentransack -y
 
 choco install fiddler -y
 choco install postman -y
