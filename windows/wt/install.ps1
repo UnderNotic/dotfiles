b25ee5685e0fb4f08928c75b6657a55c2925926b
undernotic <undernotic@gmail.com>
2021-07-23 00:39:53 +0200

    fix

--- windows/wt/install.ps1
+++ windows/wt/install.ps1
@@ -1,4 +1,4 @@
 ## right-click context setup
-Set-ExecutionPolicy Bypass -Scope Process -Force;
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+# Set-ExecutionPolicy Bypass -Scope Process -Force;
+# [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
 iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/lextm/windowsterminal-shell/master/install.ps1'))
