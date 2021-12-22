c66e4664c0008ef8d6d6f6d4b3784c3f9322a5b3
undernotic <undernotic@gmail.com>
2021-12-22 23:34:49 +0100

    added npmfile

--- node/install.sh
+++ node/install.sh
@@ -1,7 +1,14 @@
-if test ! $(which spoof)
-then
-  if test $(which npm)
-  then
-    sudo npm install spoof -g
-  fi
-fi
+#setup nvm default global packages
+echo ''
+echo 'Setuping nvm default global packages'
+echo ''
+cp ./node/default-packages ~/.nvm/
+
+# node install
+echo ''
+echo 'Installing nodejs'
+echo ''
+source ~/.nvm/nvm.sh
+
+nvm install node
+nvm use node
