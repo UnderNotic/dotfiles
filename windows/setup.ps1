b4d003171883f2cf8653f5d21410fcdb4d82f9f2
undernotic <undernotic@gmail.com>
2021-12-25 10:45:55 +0100

    Move windows workspace to profile directory

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,12 +1,12 @@
 # workspace dirs
-mkdir C:\workspace\projects
-mkdir C:\workspace\101
-mkdir C:\workspace\open_source
-mkdir C:\workspace\work
+mkdir $HOME\workspace\projects
+mkdir $HOME\workspace\101
+mkdir $HOME\workspace\open_source
+mkdir $HOME\workspace\work
 
 # git
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UnderNotic/dotfiles/windows-wsl/windows/git/.gitconfig" -OutFile $HOME\.gitconfig
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UnderNotic/dotfiles/windows-wsl/windows/git/.gitconfig-work" -OutFile "C:\workspace\work\.gitconfig-work"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UnderNotic/dotfiles/windows-wsl/windows/git/.gitconfig-work" -OutFile $HOME\workspace\work\.gitconfig-work
 
 # font install
 iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/UnderNotic/dotfiles/windows-wsl/windows/fonts/install.ps1'))
