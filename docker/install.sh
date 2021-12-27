1525f2bc047dc81f6286fb7b183c0e3060b9d288
undernotic <undernotic@gmail.com>
2021-12-27 18:43:39 +0100

    not needed docker file

--- docker/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo ''
-echo 'Now adding user to docker group (running docker without sudo)'
-echo ''
-sudo gpasswd -a $USER docker
-newgrp docker
--- macos/dock.sh
+++ macos/dock.sh
@@ -5,12 +5,10 @@ dockutil --no-restart --add "/Applications/Microsoft Edge.app"
 dockutil --no-restart --add "/Applications/Microsoft OneNote.app"
 dockutil --no-restart --add "/Applications/notes"
 dockutil --no-restart --add "/Applications/vscode"
-dockutil --no-restart --add "/Applications/rider"
-dockutil --no-restart --add "/Applications/webstorm"
-dockutil --no-restart --add "/Applications/datagrip"
-
-dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
-dockutil --no-restart --add "/System/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
+# dockutil --no-restart --add "/Applications/rider"
+# dockutil --no-restart --add "/Applications/webstorm"
+# dockutil --no-restart --add "/Applications/datagrip"
+dockutil --no-restart --add "/System/Applications/System Preferences.app"
 
 killall Dock
