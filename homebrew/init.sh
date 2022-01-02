be10cd437ec020a27a59f99974fedbe9c4bd67b1
undernotic <undernotic@gmail.com>
2022-01-02 22:59:07 +0100

    fix  brew install

--- homebrew/init.sh
+++ homebrew/init.sh
@@ -4,13 +4,13 @@ if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
   yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/undernotic/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 echo "Installing apps from Brewfile"
-# brew bundle --file=~/homebrew/Brewfile
-brew bundle --file=~/homebrew/MinimalBrewfile
+# brew bundle --file=~/.dotfiles/homebrew/Brewfile
+brew bundle --file=~/.dotfiles/homebrew/MinimalBrewfile
 
 echo "Updating and upgrading Homebrew..."; echo;
 yes | brew update
