4bde4965628cdcae56a6e48f9f0b801ca87020af
undernotic <undernotic@gmail.com>
2022-01-02 22:53:23 +0100

    add homebrew to path

--- homebrew/init.sh
+++ homebrew/init.sh
@@ -4,6 +4,8 @@ if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
   yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 echo "Installing apps from Brewfile"
