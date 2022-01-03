00ff719b809d544cc2fd5427d4b96d5f285751ca
undernotic <undernotic@gmail.com>
2022-01-03 01:32:54 +0100

    uncomment stuff

--- macos/install.sh
+++ macos/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# echo ''
-# echo 'Updating system stuff'
-# echo ''
-# sudo softwareupdate -i -a
+echo ''
+echo 'Updating system stuff'
+echo ''
+sudo softwareupdate -i -a
 
-# echo ''
-# echo 'Setting up dock'
-# echo ''
-# sh macos/dock.sh
+echo ''
+echo 'Setting up dock'
+echo ''
+sh macos/dock.sh
 
-# echo ''
-# echo 'Setting macos defaults'
-# echo ''
-# sh macos/set-defaults.sh
+echo ''
+echo 'Setting macos defaults'
+echo ''
+sh macos/set-defaults.sh
