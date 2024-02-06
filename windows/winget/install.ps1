556339d495142dc12e9e13f7462f3b0db36e19aa
undernotic <undernotic@gmail.com>
2024-02-07 00:26:57 +0100

    add sticky notes

--- windows/winget/install.ps1
+++ windows/winget/install.ps1
@@ -4,4 +4,6 @@ winget install -e --id Facebook.Messenger
 winget install -e --id Microsoft.PowerShell
 # OneNote
 winget install -e --id XPFFZHVGQWWLHB
+# OneNote Sticky Notes
+winget install -e --id 9NBLGGH4QGHW
 winget install -e --id SomePythonThings.WingetUIStore
