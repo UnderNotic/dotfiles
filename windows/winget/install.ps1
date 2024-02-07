886a92cfdb4cf4cb637b391d861ade6e751615f2
Piotr Szymura <undernotic@gmail.com>
2024-02-07 10:06:21 +0100

    Update install.ps1

--- windows/winget/install.ps1
+++ windows/winget/install.ps1
@@ -1,9 +1,11 @@
 # possibly move to chocolatey
+winget install -e --id Microsoft.PowerShell
 winget install -e --id WhatsApp.WhatsApp
 winget install -e --id Facebook.Messenger
 winget install -e --id Microsoft.PowerShell
+winget install -e --id SomePythonThings.WingetUIStore
 # OneNote
-winget install -e --id XPFFZHVGQWWLHB
+# winget install -e --id XPFFZHVGQWWLHB
 # OneNote Sticky Notes
 # winget install -e --id 9NBLGGH4QGHW
-# winget install -e --id SomePythonThings.WingetUIStore
+
