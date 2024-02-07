1b78ddcabc4091b2b74161eb4f67dd670006d881
undernotic <undernotic@gmail.com>
2024-02-07 10:48:14 +0100

    do not install fonts if they were installed already

--- windows/fonts/install.ps1
+++ windows/fonts/install.ps1
@@ -1,4 +1,11 @@
-mkdir $HOME\fonts-to-be-installed
+$Path="$HOME\fonts-to-be-installed"
+
+if (Test-Path -Path $Path) {
+    "Path already exists, not installing fonts!"
+    return
+}
+
+mkdir $Path
 Invoke-WebRequest -Uri "https://github.com/UnderNotic/dotfiles/raw/windows-wsl/windows/fonts/fonts-to-be-installed/MesloLGS%20NF%20Bold%20Italic.ttf" -OutFile $HOME\fonts-to-be-installed\MesloLGS_NF_Bold_Italic.ttf
 Invoke-WebRequest -Uri "https://github.com/UnderNotic/dotfiles/raw/windows-wsl/windows/fonts/fonts-to-be-installed/MesloLGS%20NF%20Italic.ttf" -OutFile $HOME\fonts-to-be-installed\MesloLGS_NF_Italic.ttf
 Invoke-WebRequest -Uri "https://github.com/UnderNotic/dotfiles/raw/windows-wsl/windows/fonts/fonts-to-be-installed/MesloLGS%20NF%20Bold.ttf" -OutFile $HOME\fonts-to-be-installed\MesloLGS_NF_Bold.ttf
@@ -12,7 +19,6 @@ Invoke-WebRequest -Uri "https://github.com/UnderNotic/dotfiles/raw/windows-wsl/w
 
 
 $FONTS = 0x14
-$Path="$HOME\fonts-to-be-installed"
 $objShell = New-Object -ComObject Shell.Application
 $objFolder = $objShell.Namespace($FONTS)
 $Fontdir = dir $Path
